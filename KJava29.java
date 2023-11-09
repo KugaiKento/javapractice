67419a2c16088a94fd36d6e22b5b6fe83735eb1e
Kentokugai <kd1344439@st.kobedenshi.ac.jp>
2023-11-09 09:21:32 +0900

    kadai29

--- KJava29.java
+++ KJava29.java
@@ -1,5 +1,15 @@
+import java.util.*;
 public class KJava29 {
     public static void main(String[] args) {
-        
+        // 行10列の配列に1から25以外の値が入力されるまで、1から25の範囲にある値を入力し、その後表示する。.
+        int[][] num;
+        num = new int[5][10];
+        System.out.print("1から25の数字を入力してください");
+        // 配列に10列に25以下の数字を10回入力出来たらiを１増やす
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 10; j++){
+                
+            }
+        }
     }
 }
