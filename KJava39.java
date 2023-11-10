8892e244bf4752af6dc640667dbd710ba31f3de5
Kentokugai <kd1344439@st.kobedenshi.ac.jp>
2023-11-10 18:21:41 +0900

    K39

---
+++ KJava39.java
@@ -0,0 +1,7 @@
+public class KJava39 {
+    public static void main(String[] args) {
+        for(int i = 0; i < args.length; i++){
+            System.out.printf("args[%d]=%s%n",i,args[i]);
+        }
+    }
+}
---
+++ RJava19.java
@@ -0,0 +1,13 @@
+public class RJava19 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/**
+ * InnerRJava19
+ */
+public interface InnerRJava19 {
+
+    
+}
