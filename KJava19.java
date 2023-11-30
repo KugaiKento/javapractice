ea3bfbf97193cd66624ec618b5170ebafb0ac7d3
Kentokugai <kd1344439@st.kobedenshi.ac.jp>
2023-11-30 09:27:24 +0900

    課題

--- KJava19.java
+++ KJava19.java
@@ -1,12 +1,5 @@
-import java.util.HashMap;
 public class KJava19 {
     public static void main(String[] args) {
-        HashMap<String, Integer> hmap = new HashMap<String, Integer>();
-        hmap.put("りんご", 100);
-        hmap.put("バナナ", 200);
-        hmap.put("ぶどう", 300);
-
-        //      取得
-        System.out.println(hmap.get("りんご"));
+        
     }
 }
---
+++ KJava32.java
@@ -0,0 +1,6 @@
+public class KJava32 {
+    public static void main(String[] args) {
+        int[][] num = new int[4][4];
+        
+    }
+}
