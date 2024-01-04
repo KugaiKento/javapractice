83694210da040e8b1152ce8b3450176768be8111
Kentokugai <kd1344439@st.kobedenshi.ac.jp>
2024-01-05 00:38:04 +0900

    修正

--- KJava19.java
+++
@@ -1,5 +0,0 @@
-public class KJava19 {
-    public static void main(String[] args) {
-        
-    }
-}
--- KJava32.java
+++
@@ -1,6 +0,0 @@
-public class KJava32 {
-    public static void main(String[] args) {
-        int[][] num = new int[4][4];
-        
-    }
-}
